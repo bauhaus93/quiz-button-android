f4210670a650f674f7c48cecccdd5ab579c4c81f
Jakob Fischer <jakobfischer93@gmail.com>
2024-02-24 22:20:50 +0100

    Update app, add privacy policy

--- quizbutton/src/main/java/com/shadowrulor/quizbutton/Constants.java
+++ quizbutton/src/main/java/com/shadowrulor/quizbutton/Constants.java
@@ -1,7 +1,7 @@
 package com.shadowrulor.quizbutton;
 
 public class Constants {
-    public static final int SELECTION_TIMEOUT = 100;
+    public static final int SELECTION_TIMEOUT = 500;
     public static final int TIME_BETWEEN_ACTIVATION = 3000;
     public static final int TIME_CLIENT_PLACEMENT_RESET = 10000;
     public static final int HOST_DISCOVERY_CONNECTION_TIMEOUT = 250;
